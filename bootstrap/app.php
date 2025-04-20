b8a45bc96772a44d387bcdbf8833fa24da11ba08
Salatiso <SPICEINC@GMAIL.COM>
2025-04-20 23:03:18 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,1 +1,34 @@
 
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders()
+    ->withFacades()
+    ->withExceptionHandler(function () {
+        //
+    })
+    ->withCoreMiddleware(function (Middleware $middleware) {
+        $middleware->bootSession();
+        $middleware->loadRequest();
+        $middleware->shareRequest();
+        $middleware->validateRequest();
+        $middleware->prepareForMaintenance();
+    })
+    ->withRouting(function () {
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+    })
+    ->withViewEngine()
+    ->withSession()
+    ->withQueues()
+    ->withEvents()
+    ->create();
