e8fc12fa5069f9d9c5eb7c23d57fb1bba1151302
Salatiso <SPICEINC@GMAIL.COM>
2025-04-20 23:06:50 +0200

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -1,1 +1,100 @@
+<?php
+
+use Illuminate\Support\Str;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Defaults
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the default authentication "guard" and password
+    | reset mechanisms you may use. These defaults can be changed as
+    | required, but they provide a sensible start for most applications.
+    |
+    */
+
+    'defaults' => [
+        'guard' => 'web',
+        'passwords' => 'users',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Guards
+    |--------------------------------------------------------------------------
+    |
+    | Next, you may define every authentication guard for your application.
+    | These guards define how users are authenticated for each request to
+    | your application. Supported: "session", "token".
+    |
+    */
+
+    'guards' => [
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+
+        'api' => [
+            'driver' => 'token',
+            'provider' => 'users',
+            'hash' => false,
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Providers
+    |--------------------------------------------------------------------------
+    |
+    | All authentication drivers have a user provider. This defines how the
+    | actual users are retrieved out of your database or other storage
+    | mechanisms used by this application to persist your user's data.
+    |
+    */
+
+    'providers' => [
+        'users' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\User::class,
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Resetting Passwords
+    |--------------------------------------------------------------------------
+    |
+    | You may specify the options for resetting passwords, including the view
+    | that is your password reset e-mail. You may also set the name of the
+    | table that maintains all of the reset tokens and the number of minutes
+    | your reset tokens should remain valid.
+    |
+    | Supported: "database", "redis"
+    |
+    */
+
+    'passwords' => [
+        'users' => [
+            'provider' => 'users',
+            'table' => 'password_resets',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Confirmation Timeout
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define the amount of seconds before a password confirmation
+    | times out and the user is prompted to re-enter their password.
+    |
+    */
+
+    'password_timeout' => 10800,
+];
 
