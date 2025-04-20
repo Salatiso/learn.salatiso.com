8e8897d4d71b6cccbb726df735b402a5e86bab63
Salatiso <SPICEINC@GMAIL.COM>
2025-04-20 22:53:42 +0200

    Update Learner.php

--- Models/Learner.php
+++ Models/Learner.php
@@ -1,1 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Learner extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'grade_level',
+        'date_of_birth',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
 
