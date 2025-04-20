a17e2d055ef9f8ff09b72b326a27dc64df9a4339
Salatiso <SPICEINC@GMAIL.COM>
2025-04-21 00:11:01 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Enjoy building your web!
+|
+*/
 
 // Homepage
 Route::get('/', [PageController::class, 'index'])->name('home');
@@ -16,6 +27,7 @@ Route::get('/self-driven-learning', [PageController::class, 'selfDrivenLearning'
 
 // Printable Resources
 Route::get('/printables', [PageController::class, 'printableResources'])->name('printables');
+Route::get('/printables/grade/{grade}', [PageController::class, 'showGradeResources'])->name('printables.grade');
 
 // User Dashboard
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
@@ -23,23 +35,9 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
 // Support
 Route::get('/support', [PageController::class, 'showSupport'])->name('support');
 
-// Login
+// Authentication
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-
-// Register
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
-
-//Logout
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-// Privacy Policy
-Route::get('/privacy-policy', function () {
-    return view('privacy-policy');
-})->name('privacy-policy');
-
-// Terms of Service
-Route::get('/terms-of-service', function () {
-    return view('terms-of-service');
-})->name('terms-of-service');
