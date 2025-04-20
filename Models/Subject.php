c5b453c3ba2ca8dad55a30bb2990251b97261b10
Salatiso <SPICEINC@GMAIL.COM>
2025-04-20 22:55:56 +0200

    Update Subject.php

--- Models/Subject.php
+++ Models/Subject.php
@@ -1,1 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Subject extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'subject_name',
+        'subject_type',
+        'description'
+    ];
+
+      public function lessons(): HasMany
+    {
+        return $this->hasMany(Lesson::class, 'subject_id');
+    }
+
+    public function assessments(): HasMany
+    {
+        return $this->hasMany(Assessment::class, 'subject_id');
+    }
+}
 
