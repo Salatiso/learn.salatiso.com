6f4500bff56944fb37786060b0b7cd60c881234c
Salatiso <SPICEINC@GMAIL.COM>
2025-04-20 23:50:34 +0200

    Update validation.php

--- resources/lang/nso/validation.php
+++ resources/lang/nso/validation.php
@@ -1,1 +1,207 @@
+**1. Create the Directory**
 
+First, create a directory for the Sepedi language files within your `resources/lang` directory:
+
+```
+resources/
+└── lang/
+    └── nso/  <-- Create this directory
+        ├── auth.php
+        ├── pagination.php
+        ├── passwords.php
+        ├── validation.php
+        └── messages.php  <-- You can create more files as needed
+```
+
+**2. Create the Language Files**
+
+Now, create the following PHP files within the `nso` directory and add the corresponding Sepedi translations.
+
+**resources/lang/nso/auth.php**
+
+```php
+<?php
+
+return [
+    'failed' => 'Ga di nyalelane le mangwalo a go hlatsela a rena.',
+    'password' => 'Password ga e nepagale.',
+    'throttle' => 'Mekgwa e mentši haholo. Ka kopo leka gape ka mor\'a metsotsoana e :seconds.',
+    'login' => 'Tsenya',
+    'register' => 'Ngwadiša',
+    'name' => 'Leina',
+    'email' => 'Imeile',
+    'password' => 'Password',
+    'confirm_password' => 'Qinisekisa Password',
+    'remember_me' => 'Nkgopole',
+    'forgot_your_password' => 'O lebetše password ya gago?',
+    'reset_password' => 'Seta Botsjha Password',
+    'send_password_reset_link' => 'Romela kgokagano ya go seta botsjha password',
+    'old_password' => 'Password ya Kgale',
+    'new_password' => 'Password e Ntjha',
+    'confirm_new_password' => 'Qinisekisa Password e Ntjha',
+    'update_password' => 'Mpshafatša Password',
+];
+```
+
+**resources/lang/nso/pagination.php**
+
+```php
+<?php
+
+return [
+    'previous' => '&laquo; E fetilego',
+    'next' => 'E latelago &raquo;',
+];
+```
+
+**resources/lang/nso/passwords.php**
+
+```php
+<?php
+
+return [
+    'reset' => 'Password ya gago e hlomilwe botjha!',
+    'sent' => 'Re rometse sehokelo sa ho seta botsjha password ya gago ka imeile!',
+    'throttled' => 'Ka kopo leta pele o leka hape.',
+    'token' => 'Token ena ya ho seta botsjha password ga se ya nepa.',
+    'user' => "Ha re kgone ho fumana mosebelisi ya nang le aterese ena ya imeile.",
+];
+```
+
+**resources/lang/nso/validation.php**
+
+```php
+<?php
+
+return [
+    'accepted' => 'The :attribute e tlameha ho amohelwa.',
+    'accepted_if' => 'The :attribute e tlameha ho amohelwa ha :other e le :value.',
+    'active_url' => 'The :attribute hase URL e sebetsang.',
+    'after' => 'The :attribute e tlameha ho ba letsatsi kamora :date.',
+    'after_or_equal' => 'The :attribute e tlameha ho ba letsatsi kamora kapa ho lekana le :date.',
+    'alpha' => 'The :attribute e ka ba le ditlhaku feela.',
+    'alpha_dash' => 'The :attribute e ka ba le ditlhaku, dinomoro, le makhutšoanyane feela.',
+    'alpha_num' => 'The :attribute e ka ba le ditlhaku le dinomoro feela.',
+    'array' => 'The :attribute e tlameha ho ba lethathamo.',
+    'before' => 'The :attribute e tlameha ho ba letsatsi pele ho :date.',
+    'before_or_equal' => 'The :attribute e tlameha ho ba letsatsi pele kapa ho lekana le :date.',
+    'between' => [
+        'numeric' => 'The :attribute e tlameha ho ba pakeng tsa :min le :max.',
+        'file' => 'The :attribute e tlameha ho ba pakeng tsa :min le :max kilobytes.',
+        'string' => 'The :attribute e tlameha ho ba pakeng tsa :min le :max di-character.',
+        'array' => 'The :attribute e tlameha ho ba le pakeng tsa :min le :max dintho.',
+    ],
+    'boolean' => 'Tšimo ea :attribute e tlameha ho ba nnete kapa leshano.',
+    'confirmed' => 'Netefatso ea :attribute ha e tšoane.',
+    'current_password' => 'Password ha e nepahetseng.',
+    'date' => 'The :attribute hase letsatsi le nepahetseng.',
+    'date_equals' => 'The :attribute e tlameha ho ba letsatsi le lekanang le :date.',
+    'date_format' => 'The :attribute ha e tsamellane le sebopeho :format.',
+    'declined' => 'The :attribute e tlameha ho hanoa.',
+    'declined_if' => 'The :attribute e tlameha ho hanoa ha :other e le :value.',
+    'different' => 'The :attribute le :other li tlameha ho fapana.',
+    'digits' => 'The :attribute e tlameha ho ba lipalo tse :digits.',
+    'digits_between' => 'The :attribute e tlameha ho ba pakeng tsa :min le :max lipalo.',
+    'email' => 'The :attribute e tlameha ho ba aterese ya imeile e nepahetseng.',
+    'ends_with' => 'The :attribute e tlameha ho qala ka e 'ngoe ho tse latelang: :values.',
+    'enum' => 'The :attribute e khethiloeng ha e nepahetseng.',
+    'exists' => 'The :attribute e khethiloeng ha e nepahetseng.',
+    'file' => 'The :attribute e tlameha ho ba faele.',
+    'filled' => 'Tšimo ea :attribute e tlameha ho ba le boleng.',
+     'gt' => [
+        'numeric' => 'The :attribute e tlameha ho ba kholo ho :value.',
+        'file' => 'The :attribute e tlameha ho ba kholo ho :value kilobytes.',
+        'string' => 'The :attribute e tlameha ho ba kholo ho :value characters.',
+        'array' => 'The :attribute e tlameha ho ba le lintho tse fetang :value.',
+    ],
+    'gte' => [
+        'numeric' => 'The :attribute e tlameha ho ba kholo kapa ho lekana le :value.',
+        'file' => 'The :attribute e tlameha ho ba kholo kapa ho lekana le :value kilobytes.',
+        'string' => 'The :attribute e tlameha ho ba kholo kapa ho lekana le :value characters.',
+        'array' => 'The :attribute e tlameha ho ba le lintho tse ngata kapa tse lekanang le :value.',
+    ],
+    'image' => 'The :attribute e tlameha ho ba setshwantsho.',
+    'in' => 'The :attribute e khethiloeng ha e nepahetseng.',
+    'in_array' => 'Insimu ea :attribute ha e yo ho :other.',
+    'integer' => 'The :attribute e tlameha ho ba palo e felletseng.',
+    'ip' => 'The :attribute e tlameha ho ba aterese ea IP e nepahetseng.',
+    'ipv4' => 'The :attribute e tlameha ho ba aterese ea IPv4 e nepahetseng.',
+    'ipv6' => 'The :attribute e tlameha ho ba aterese ea IPv6 e nepahetseng.',
+    'json' => 'The :attribute e tlameha ho ba khoele e nepahetseng ea JSON.',
+    'lt' => [
+        'numeric' => 'The :attribute e tlameha ho ba tlase ho :value.',
+        'file' => 'The :attribute e tlameha ho ba tlase ho :value kilobytes.',
+        'string' => 'The :attribute e tlameha ho ba tlase ho :value characters.',
+        'array' => 'The :attribute e tlameha ho ba le lintho tse fokolang ho :value.',
+    ],
+    'lte' => [
+        'numeric' => 'The :attribute e tlameha ho ba tlase kapa ho lekana le :value.',
+        'file' => 'The :attribute e tlameha ho ba tlase kapa ho lekana le :value kilobytes.',
+        'string' => 'The :attribute e tlameha ho ba tlase kapa ho lekana le :value characters.',
+        'array' => 'The :attribute e tlameha ho ba le lintho tse fokolang kapa tse lekanang le :value.',
+    ],
+    'mac_address' => 'The :attribute e tlameha ho ba aterese ea MAC e nepahetseng.',
+    'max' => [
+        'numeric' => 'The :attribute e kanne ya se be kgolo ho :max.',
+        'file' => 'The :attribute e kanne ya se be kgolo ho :max kilobytes.',
+        'string' => 'The :attribute e kanne ya se be kgolo ho :max characters.',
+        'array' => 'The :attribute e kanne ya se be le lintho tse fetang :max.',
+    ],
+    'mimes' => 'The :attribute e tlameha ho ba faele ya mofuta: :values.',
+    'mimetypes' => 'The :attribute e tlameha ho ba faele ya mofuta: :values.',
+    'min' => [
+        'numeric' => 'The :attribute kumele ibe ncane kune :min.',
+        'file' => 'The :attribute kumele ibe ncane kune :min kilobytes.',
+        'string' => 'The :attribute kumele ibe ncane kune :min abalinganiswa.',
+        'array' => 'I :attribute kumele ibe nezinto ezingaphezu kwe :min.',
+    ],
+    'multiple_of' => 'The :attribute kumele ibe isiphindaphindo se :value.',
+    'not_in' => 'The :attribute ekhethiweyo ayilungile.',
+    'not_regex' => 'Ifomethi ye :attribute ayilungile.',
+    'nullable' => 'Insimu ye :attribute ingaba null.',
+    'numeric' => 'The :attribute kumele kube inombolo.',
+    'password' => 'Iphasiwedi ayilungile.',
+    'present' => 'Insimu ye :attribute kumele ibekhona.',
+    'prohibited' => 'Insimu ye :attribute ayivunyelwe.',
+    'prohibited_if' => 'Insimu ye :attribute ayivunyelwe lapho i :other iyiyo :value.',
+    'prohibited_unless' => 'Insimu ye :attribute ayivunyelwe ngaphandle kokuthi i :other iku :values.',
+    'regex' => 'Ifomethi ye :attribute ayilungile.',
+    'required' => 'Insimu ye :attribute iyadingeka.',
+    'required_if' => 'Insimu ye :attribute iyadingeka lapho i :other iyiyo :value.',
+    'required_unless' => 'Insimu ye :attribute iyadingeka ngaphandle kokuthi i :other iku :values.',
+    'required_with' => 'Insimu ye :attribute iyadingeka lapho kukhona i :values.',
+    'required_with_all' => 'Insimu ye :attribute iyadingeka lapho kukhona i :values.',
+    'required_without' => 'Insimu ye :attribute iyadingeka lapho kungekho i :values.',
+    'required_without_all' => 'Insimu ye :attribute iyadingeka lapho kungekho nanye kwezi :values.',
+    'same' => 'I :attribute ne :other kumele zifane.',
+    'size' => [
+        'numeric' => 'I :attribute kumele ibe ngu :size.',
+        'file' => 'I :attribute kumele ibe ngu :size kilobytes.',
+        'string' => 'I :attribute kumele ibe ngu :size abalinganiswa.',
+        'array' => 'I :attribute kumele ibe nezinto ezingama :size.',
+    ],
+    'starts_with' => 'I :attribute kumele iqale ngokulandelayo: :values.',
+    'string' => 'I :attribute kumele kube uchungechunge.',
+    'timezone' => 'I :attribute kumele ibe yindawo yesikhathi esebenzayo.',
+    'unique' => 'I :attribute isivele ithathiwe.',
+    'uploaded' => 'I :attribute yehlulekile ukulayisha.',
+    'url' => 'Ifomethi ye :attribute ayilungile.',
+    'uuid' => 'I :attribute kumele kube yi-UUID evumelekile.',
+    'custom' => [
+        'email' => [
+            'required' => 'Ikheli le-imeyili liyadingeka.',
+            'email' => 'Faka ikheli le-imeyili elivumelekile.',
+        ],
+        'password' => [
+            'required' => 'Iphasiwedi iyadingeka.',
+            'min' => 'Iphasiwedi kumele ibe nobuncane bezinhlamvu eziyi-8.',
+            'confirmed' => 'Iphasiwedi ayihambisani nokuqinisekiswa.',
+        ],
+    ],
+    'attributes' => [
+        'name' => 'igama',
+        'email' => 'i-imeyili',
+        'password' => 'iphasiwedi',
+    ],
+];
+```
