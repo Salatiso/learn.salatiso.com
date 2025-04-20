0f91b396dd3de3f1b55a38c45f640ee0c15d47dd
Salatiso <SPICEINC@GMAIL.COM>
2025-04-20 22:57:55 +0200

    Update UserProgress.php

--- Models/UserProgress.php
+++ Models/UserProgress.php
@@ -1,1 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserProgress extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'lesson_id',
+        'assessment_id',
+        'progress',
+        'score',
+        'completion_date',
+    ];
+
+     public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function lesson(): BelongsTo
+    {
+        return $this->belongsTo(Lesson::class, 'lesson_id');
+    }
+
+      public function assessment(): BelongsTo
+    {
+         return $this->belongsTo(Assessment::class, 'assessment_id');
+    }
+}
 
