8ce65327272cbddfe505a49c7d4b6973ab19e09d
Salatiso <SPICEINC@GMAIL.COM>
2025-04-20 23:25:03 +0200

    Update ####_create_user_progress_table.php

--- database/migrations/####_create_user_progress_table.php
+++ database/migrations/####_create_user_progress_table.php
@@ -1,1 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProgressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_progress', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('lesson_id')->nullable();
+            $table->unsignedBigInteger('assessment_id')->nullable();
+            $table->integer('progress')->nullable();
+            $table->integer('score')->nullable();
+            $table->timestamp('completion_date')->nullable();
+            $table->primary(['user_id', 'lesson_id', 'assessment_id']);
+            $table->timestamps(); // created_at and updated_at
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('lesson_id')->references('id')->on('lessons')->onDelete('cascade');
+            $table->foreign('assessment_id')->references('id')->on('assessments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_progress');
+    }
+}
 
